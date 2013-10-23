8729e04ac5cef7abd7987565cbcacdec3caf2a46
MAYAH <mayah@mayah.jp>
2013-10-23 13:19:23 +0900

    We should read MacPorts settings only on Mac.

--- .zsh/zprofile.d/editor.zsh
+++ .zsh/zprofile.d/editor.zsh
@@ -1,6 +1,9 @@
 # -*- mode: shell-script -*-
 
-if which vi >& /dev/null ; then
+if which vi >& /dev/null; then
     export EDITOR=vi
+fi
+
+if which lv >& /dev/null; then
     export GIT_PAGER="lv -c"
 fi
--- .zsh/zprofile.d/path.zsh
+++ .zsh/zprofile.d/path.zsh
@@ -1,31 +1,32 @@
 # -*- mode: shell-script -*-
 
-#export PATH=~/repos/chromium.git:$PATH
-#export PATH=~/repos/chromium.git/src/third_party/llvm-build/Release+Asserts/bin:$PATH
-#export PATH=/usr/texbin/:$PATH
-#export PATH=/usr/local/bin:$PATH
+case `uname` in
+    Darwin)
+        # Prepare MacPorts PATHs.
+        export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+        export MANPATH=/opt/local/man:$MANPATH
+        export LIBRARY_PATH=/opt/local/lib:$LIBRARY_PATH
+        export LD_LIBRARY_PATH=/opt/local/lib:$LD_LIBRARY_PATH
 
-# For MacPorts environment, we would like to use the following.
-# TODO(mayah): We have to limit these settings only for MacOSX?
-export PATH=/Library/PostgreSQL/9.1/bin:$PATH
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
-export MANPATH=/opt/local/man:$MANPATH
-export LIBRARY_PATH=/opt/local/lib:$LIBRARY_PATH
-export LD_LIBRARY_PATH=/opt/local/lib:$LD_LIBRARY_PATH
+        # If C_INCLUDE_PATH is like "/opt/local/include:", gcc searches "." also.
+        # This caused a problem that my compiler test fails!
+        if [ -n "$C_INCLUDE_PATH" ]; then
+            export C_INCLUDE_PATH=/opt/local/include:$C_INCLUDE_PATH
+        else
+            export C_INCLUDE_PATH=/opt/local/include
+        fi
 
-# If C_INCLUDE_PATH is like "/opt/local/include:", gcc searches "." also.
-# This caused a problem that my compiler test fails!
-if [ -n "$C_INCLUDE_PATH" ]; then
-    export C_INCLUDE_PATH=/opt/local/include:$C_INCLUDE_PATH
-else
-    export C_INCLUDE_PATH=/opt/local/include
-fi
+        if [ -n "$CPLUS_INCLUDE_PATH" ]; then
+            export CPLUS_INCLUDE_PATH=/opt/local/include:$CPLUS_INCLUDE_PATH
+        else
+            export CPLUS_INCLUDE_PATH=/opt/local/include
+        fi
 
-if [ -n "$CPLUS_INCLUDE_PATH" ]; then
-    export CPLUS_INCLUDE_PATH=/opt/local/include:$CPLUS_INCLUDE_PATH
-else
-    export CPLUS_INCLUDE_PATH=/opt/local/include
-fi
+        export PATH=/Library/PostgreSQL/9.1/bin:$PATH
+        ;;
+    *)
+        ;;
+esac
 
 # I often put my personal binaries into my home directory.
 export PATH=~/usr/depot_tools:$PATH
