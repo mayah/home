95a6877b7a2cfbe1d97b636c473c073b888a291c
MAYAH <mayah@mayah.jp>
2014-07-15 11:28:56 +0900

    Add several PATHs.

--- .zsh/zprofile.d/path.zsh
+++ .zsh/zprofile.d/path.zsh
@@ -15,5 +15,14 @@ if [ -d /usr/local/opt/go/libexec/bin ]; then
     export PATH=/usr/local/opt/go/libexec/bin:$PATH
 fi
 
+if [ -d ~/opt/go_appengine ]; then
+    export PATH=~/opt/go_appengine:$PATH
+fi
+
+if [ -d ~/opt/bin ]; then
+    export PATH=~/opt/bin:$PATH
+fi
+
 # I often put my personal binaries into my home directory.
 export PATH=~/usr/depot_tools:~/usr/bin:~/usr/script:$PATH
+
