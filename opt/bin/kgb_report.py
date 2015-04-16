8185b39118f7cc1786afce0a8d78ce2409713dde
MAYAH <mayah@mayah.jp>
2015-04-16 12:40:34 +0900

    Install kgb_report always.

---
+++ opt/bin/kgb_report.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+# How to use:
+#   kgb_report.py <type> <content>
+#   For exmaple:
+#     kgb_report.py commands hoge
+
+import commands
+import json
+import os
+import sys
+import time
+
+if "HOME" in os.environ:
+    HOME = os.environ["HOME"]
+elif os.path.exists('/home/shinyak'):
+    HOME = '/home/shinyak'
+elif os.path.exists('/home/mayah'):
+    HOME = '/home/mayah'
+elif os.path.exists('/Users/mayah'):
+    HOME = '/Users/mayah'
+else:
+    HOME = '/Users/shinyak'
+    
+SHORTHOST = commands.getoutput('hostname -s')
+
+class KGBReport:
+    def __init__(self):
+        pass
+
+    def add(self, filename, obj):
+        # Ensures the written directory exists.
+        dirname = os.path.dirname(filename)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+        # Opens the log file.
+        if os.path.isfile(filename):
+            f = open(filename, 'a')
+        else:
+            f = open(filename, 'w')
+
+        # Writes object.
+        f.write(json.dumps(obj) + '\n')
+        f.close()
+
+    def report(self):
+        type = sys.argv[1]
+        content = sys.argv[2]
+        host = SHORTHOST
+        now = time.time()
+        date = time.strftime('%Y%m%d')
+
+        obj = {
+            'host': host,
+            'type': type,
+            'ctnt': content,
+            'time': now
+        }
+
+        filename = HOME + '/.kgb/log/' + date
+        self.add(filename, obj)
+
+if __name__ == "__main__":
+    KGBReport().report()
