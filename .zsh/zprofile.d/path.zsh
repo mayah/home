afb2e132fb6d666922ce25efe0e6253ae95e8705
MAYAH <mayah@mayah.jp>
2015-08-18 14:27:44 +0900

    Add more PATHs.

--- .zsh/zprofile.d/path.zsh
+++ .zsh/zprofile.d/path.zsh
@@ -6,17 +6,6 @@ function add_path_if_any() {
     fi
 }
 
-case `uname` in
-    Linux)
-        # Prepare for Google Cloud SDK if any.
-        if [ -d $HOME/google-cloud-sdk/bin ]; then
-            export PATH=$HOME/google-cloud-sdk/bin:$PATH
-        fi
-        ;;
-    *)
-        ;;
-esac
-
 add_path_if_any "/usr/local/opt/go/libexec/bin"
 add_path_if_any "$HOME/opt/go_appengine"
 add_path_if_any "$HOME/opt/bin"
@@ -24,3 +13,9 @@ add_path_if_any "$HOME/opt/depot_tools"
 add_path_if_any "$HOME/usr/bin"
 add_path_if_any "$HOME/usr/script"
 add_path_if_any "$HOME/homebrew/bin"
+add_path_if_any "$HOME/Android/Sdk/platform-tools"
+add_path_if_any "$HOME/Android/Sdk/tools"
+add_path_if_any "$HOME/google-cloud-sdk/bin"
+
+# remove google3 JDK for android work
+export PATH=${PATH/\/usr\/local\/buildtools\/java\/jdk\/bin:/}
