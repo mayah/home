74845d544985f14e2604829cd651d09838fe5294
MAYAH <mayah@mayah.jp>
2016-07-16 23:34:19 +0900

    Add .cargo/bin in PATH.

--- .zsh/zprofile.d/path.zsh
+++ .zsh/zprofile.d/path.zsh
@@ -16,6 +16,7 @@ add_path_if_any "$HOME/homebrew/bin"
 add_path_if_any "$HOME/Android/Sdk/platform-tools"
 add_path_if_any "$HOME/Android/Sdk/tools"
 add_path_if_any "$HOME/google-cloud-sdk/bin"
+add_path_if_any "$HOME/.cargo/bin"
 
 # remove google3 JDK for android work
 export PATH=${PATH/\/usr\/local\/buildtools\/java\/jdk\/bin:/}
