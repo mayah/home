2c5566178a5162205665e189870e2fadd0358f1b
MAYAH <mayah@mayah.jp>
2016-08-31 16:27:29 +0900

    Update path
    Remove $HOME/usr
    Move $HOME/google-cloud-sdk/bin to $HOME/opt/google-cloud-sdk/bin

--- .zsh/zprofile.d/path.zsh
+++ .zsh/zprofile.d/path.zsh
@@ -7,15 +7,13 @@ function add_path_if_any() {
 }
 
 add_path_if_any "/usr/local/opt/go/libexec/bin"
-add_path_if_any "$HOME/opt/go_appengine"
 add_path_if_any "$HOME/opt/bin"
 add_path_if_any "$HOME/opt/depot_tools"
-add_path_if_any "$HOME/usr/bin"
-add_path_if_any "$HOME/usr/script"
+add_path_if_any "$HOME/opt/go_appengine"
+add_path_if_any "$HOME/opt/google-cloud-sdk/bin"
 add_path_if_any "$HOME/homebrew/bin"
 add_path_if_any "$HOME/Android/Sdk/platform-tools"
 add_path_if_any "$HOME/Android/Sdk/tools"
-add_path_if_any "$HOME/google-cloud-sdk/bin"
 add_path_if_any "$HOME/.cargo/bin"
 
 # remove google3 JDK for android work
