fd7bf20b8bcd8e62111eb98a4a4bf3ef8d3b5a87
mayah <mayah@mayah.jp>
2019-04-08 18:54:05 +0900

    Add path.zsh

--- .zsh/zprofile.d/path.zsh
+++ .zsh/zprofile.d/path.zsh
@@ -8,6 +8,8 @@ function add_path_if_any() {
 
 add_path_if_any "/usr/local/opt/go/libexec/bin"
 add_path_if_any "/usr/local/texlive/2016/bin/x86_64-darwin"
+add_path_if_any "/usr/local/opt/openssl/bin"
+add_path_if_any "/usr/local/opt/mysql-client/bin"
 add_path_if_any "$HOME/opt/bin"
 add_path_if_any "$HOME/opt/go/bin"
 add_path_if_any "$HOME/opt/depot_tools"
@@ -17,6 +19,7 @@ add_path_if_any "$HOME/homebrew/bin"
 add_path_if_any "$HOME/Android/Sdk/platform-tools"
 add_path_if_any "$HOME/Android/Sdk/tools"
 add_path_if_any "$HOME/.cargo/bin"
+add_path_if_any "$HOME/.nodebrew/current/bin"
 
 # remove google3 JDK for android work
 export PATH=${PATH/\/usr\/local\/buildtools\/java\/jdk\/bin:/}
